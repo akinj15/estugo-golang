96e24fbc5bb725ac7f1f0fb633ce0867ffdf2b46
akin <akin.bezerra123@gmail.com>
2024-03-24 18:57:42 -0300

    att

---
+++ context/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	bookHotel(ctx)
+}
+
+func bookHotel(ctx context.Context) {
+	select {
+	case <-ctx.Done():
+		fmt.Println("Hotal booking cancelled. timeout reached.")
+	case <-time.After(5 * time.Second):
+		fmt.Println("Hotel booked")
+	}
+}
+
